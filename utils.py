35064654c5406c612510d125d4839f3b909d9300
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-10-30 09:14:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -127,7 +127,7 @@ async def broadcast_messages(user_id, message):
 def get_size(size):
     """Get size in readable format"""
 
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    units = "Bytes", "KB", "MB", "GB", "TB", "PB", "EB"
     size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
